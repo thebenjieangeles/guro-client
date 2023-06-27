ee97f9d44db899b6fb706f3f731c9abee398e2bd
obetdelacruz <ldelacruz@gbox.adnu.edu.ph>
2023-06-27 23:26:02 +0800

    update of not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,9 @@
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="text-center flex flex-col align-items-center text-6xl">
+      Not Found
+    </div>
+  );
 }
 
 export default NotFound;
