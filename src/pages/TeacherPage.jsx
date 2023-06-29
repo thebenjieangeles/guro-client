f97df915745fa67ad739523f5a725d13f6f93d6b
obetdelacruz <ldelacruz@gbox.adnu.edu.ph>
2023-06-29 13:12:24 +0800

    update teacher

--- src/pages/TeacherPage.jsx
+++ src/pages/TeacherPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import homePage from "../assets/homePage.png";
+import questionPage from "../assets/questionPage.png";
 import axios from "axios";
 
 function TeacherPage() {
@@ -19,7 +19,14 @@ function TeacherPage() {
 
   return (
     <div>
-      <div className="bg-blue-800 h-[79rem] flex flex-col justify-center items-center gap-6">
+      <div className=" bg-blue-800 h-[72rem] flex flex-col justify-center items-center gap-6 2xl:flex-row 2xl:h-[77rem] 2xl:justify-start">
+        <div className="">
+          <img
+            src={questionPage}
+            alt="Contact Page"
+            className=" w-[23rem] h-[23rem] md:w-[35rem] md:h-[35rem] lg:w-[40rem] lg:h-[40rem] 2xl:ml-[30rem] m-auto"
+          />
+        </div>
         <div className="w-3/4 lg:w-1/2">
           {questions.map((question) => (
             <div key={question._id} className="bg-white p-4 mb-4 rounded">
@@ -28,10 +35,10 @@ function TeacherPage() {
           ))}
         </div>
 
-        <form className="w-3/4 lg:w-1/2 bg-white p-4 rounded">
+        <form className="text-center bg-white p-4 rounded w-[20rem] md:w-[45rem] lg:w[60rem] 2xl:w-[80rem] 2xl:mr-[30rem]">
           <h2 className="text-2xl font-bold mb-4">Add a New Question</h2>
           <div className="mb-4">
-            <label htmlFor="questionText" className="block font-bold">
+            <label htmlFor="questionText" className="block font-bold text-left">
               Question Text:
             </label>
             <input
